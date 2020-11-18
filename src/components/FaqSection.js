2f7392422ae31b0ae8d35616c8ee7953c3649e1e
Jérome Roche <jerome.roche.2022@ensta-paris.fr>
2020-11-18 19:27:53 +0100

    FaqSection

---
+++ src/components/FaqSection.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const FaqSection = () => {
+    return(
+        <div className="faq">
+            <h2>Any Questions <span>FAQ</span></h2>
+            <div className="questions">
+                <div className="answer">
+                    <h4>How do I Start ?</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit ex quidem amet, sequi aliquid ab.</p>
+                </div>
+                <div className="answer">
+                    <h4>Daily Schedule</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit ex quidem amet, sequi aliquid ab.</p>
+                </div>
+                <div className="answer">
+                    <h4>Different Payment Method</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit ex quidem amet, sequi aliquid ab.</p>
+                </div>
+                <div className="answer">
+                    <h4>What product do you offer</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit ex quidem amet, sequi aliquid ab.</p>
+                </div>
+            </div>
+        </div>
+    )
+};
+
+export default FaqSection;
