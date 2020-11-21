5421c7fe68aa844beadcfc7997b004fae73968f4
Jérome Roche <jerome.roche.2022@ensta-paris.fr>
2020-11-21 20:39:06 +0100

    Our Work

--- src/App.js
+++ src/App.js
@@ -4,13 +4,31 @@ import GlobalStyle from './components/GlobalStyle';
 //Import Pages
 import AboutUs from './pages/AboutUs';
 import Nav from './components/Nav';
+import ContactUs from './pages/ContactUs';
+import OurWork from './pages/OurWork';
+import MovieDetail from './pages/MovieDetail';
+//Router
+import {Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Nav/>
-      <AboutUs />    
+      <Switch>
+        <Route path="/" exact>
+          <AboutUs />
+        </Route>
+        <Route path="/work" exact>
+          <OurWork/>
+        </Route>
+        <Route path="/work/:id">
+          <MovieDetail/>
+        </Route>
+        <Route path="/contact" >
+          <ContactUs/>
+        </Route>
+      </Switch>
     </div>
   )
 }
