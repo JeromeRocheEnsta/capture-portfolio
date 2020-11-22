00d72bc1657930938f8fab7c92d465b0ec6f76cb
Jérome Roche <jerome.roche.2022@ensta-paris.fr>
2020-11-22 09:53:36 +0100

    Animation

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -3,33 +3,44 @@ import home1 from '../img/home1.png';
 //Styled
 import styled from 'styled-components';
 import {About, Description, Image, Hide} from "../style"
-
+//Framer Motion
+import {motion} from "framer-motion";
+import {titleAnim, fade, photoAnim} from "../animation";
+import Wave from "./Wave";
 
 const AboutSection = () => {
+
     return (
         <About>
             <Description>
-                <div className="title">
+                <motion.div className="title">
                     <Hide>
-                        <h2>We work to make</h2>
+                        <motion.h2 variants={titleAnim}>
+                            We work to make
+                        </motion.h2>
                     </Hide>
                     <Hide>
-                        <h2>
+                        <motion.h2 variants={titleAnim}>
                             Your <span>dreams</span> come
-                        </h2>
+                        </motion.h2>
                     </Hide>
                     <Hide>
-                        <h2>true.</h2>
+                        <motion.h2 variants={titleAnim}>
+                            true.
+                        </motion.h2>
                     </Hide>
-                </div>
-                <p>Contact us for any photography or videography ideas that you have. We have professionals with amazing skills</p>
-                <button>Contact Us</button>
+                </motion.div>
+                <motion.p variants={fade}>Contact us for any photography or videography ideas that you have. We have professionals with amazing skills</motion.p>
+                <motion.button variants={fade}>Contact Us</motion.button>
             </Description>
             <Image>
-                <img src={home1} alt="guy with a camera"/>
+                <motion.img variants={photoAnim}  src={home1} alt="guy with a camera"/>
             </Image>
+            <Wave />
         </About>
     )
 };
+
+
 
 export default AboutSection;
